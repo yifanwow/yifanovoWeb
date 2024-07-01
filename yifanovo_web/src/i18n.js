966cdcacc041aff1b4cec6cabe9c145e10e3a661
yifanwow <yifanovo@gmail.com>
2024-07-01 15:12:14 -0400

    adjust the name of jsx component.

--- yifanovo_web/src/i18n.js
+++ yifanovo_web/src/i18n.js
@@ -1,29 +1,29 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 // Language files
-import translationEN from './locales/en/translation.json';
-import translationZH from './locales/zh/translation.json';
+import translationEN from "./locales/en/translation.json";
+import translationZH from "./locales/zh/translation.json";
 
 // the translations
 const resources = {
   en: {
-    translation: translationEN
+    translation: translationEN,
   },
   zh: {
-    translation: translationZH
-  }
+    translation: translationZH,
+  },
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en', // default language
+    lng: "en", // default language
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
