505f4b10deb791ad1bdfacf520a89645d9eb398e
yifanwow <yifanovo@gmail.com>
2024-07-07 23:52:52 -0400

    Separated the email content used for validation from emailRoutes, wrote a separate html file to optimize the effect, and is currently working on solving the problem that svg can't be displayed correctly in some email systems.

--- server/routes/emailRoutes.js
+++ server/routes/emailRoutes.js
@@ -1,35 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const createTransporter = require('../mailer');
-const uuid = require('uuid').v4;
+const createTransporter = require("../mailer");
+const uuid = require("uuid").v4;
+const fs = require("fs");
+const path = require("path");
 
-router.post('/submit-email', async (req, res) => {
+router.post("/submit-email", async (req, res) => {
   try {
     const transporter = await createTransporter();
     if (!transporter) {
-      return res.status(500).send({ message: 'Failed to create email transporter.' });
+      return res
+        .status(500)
+        .send({ message: "Failed to create email transporter." });
     }
 
     const { email } = req.body;
     const verificationToken = uuid();
+
+    // 读取 HTML 模板文件
+    const emailTemplatePath = path.join(__dirname, "emailTemple.html");
+    let emailTemplate = fs.readFileSync(emailTemplatePath, "utf8");
+
+    // 替换模板中的占位符
+    emailTemplate = emailTemplate.replace(/{{token}}/g, verificationToken);
+
     const mailOptions = {
       from: `Yifan Yu<yifanoxo@gmail.com>`,
       to: email,
-      subject: 'Email Verification-Yifanovo',
-      text: `您好，感谢您的关注！\n\n请点击以下链接以验证您的邮箱地址以激活关注：[测试]，如果您没有订阅，请忽略此邮件。\n\nHi, thank you for your interest! \n\nPlease click on the following link to verify your email: [测试], if you are not subscribed, please ignore this email.\n\n-饭饭 Yifan Yu\nhttps://yifanovo.info`,
+      subject: "Email Verification / 邮箱订阅验证 - Yifanovo",
+      html: emailTemplate, // 使用 HTML 内容替换文本内容
     };
 
     await transporter.sendMail(mailOptions);
-    res.send({ message: 'Verification email sent.' });
+    res.send({ message: "Verification email sent." });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: 'Error sending verification email.', error: error.message });
+    res.status(500).send({
+      message: "Error sending verification email.",
+      error: error.message,
+    });
   }
 });
 
-router.get('/verify-email', async (req, res) => {
+router.get("/verify-email", async (req, res) => {
   const { token } = req.query;
-  res.send('Email verified successfully!');
+  res.send("Email verified successfully!");
 });
 
 module.exports = router;
